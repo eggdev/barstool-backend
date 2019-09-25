1c98bb0cea2f2b6a1f58b5d97d868187cd283a89
Brendan Eggers <eggers.brendan@gmail.com>
2019-09-25 09:10:49 -0400

    Getting set up on work machine

--- server/app.js
+++ server/app.js
@@ -9,5 +9,5 @@ app.use(BodyParser.urlencoded({ extended: true }));
 mongoose.connect('mongodb://localhost/foreplay-gods-are-cool', { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.listen(3000, () => {
-    console.log("Listening at :3000...");
+    console.log("Listening at :3000");
 });
